c6a0edf6b7636badb6341e824089dee7333efacc
Willy-WZW <joystick449@gmail.com>
2024-11-23 14:04:16 +0800

    new project

---
+++ src/main/java/com/social_media_back/SocialMediaBackApplication.java
@@ -0,0 +1,13 @@
+package com.social_media_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaBackApplication.class, args);
+	}
+
+}
