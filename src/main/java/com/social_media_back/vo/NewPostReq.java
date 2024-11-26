78cc4086ac6d8a57cb15076be6668410083cbaac
Willy-WZW <joystick449@gmail.com>
2024-11-26 15:17:50 +0800

    小更新

--- src/main/java/com/social_media_back/vo/NewPostReq.java
+++ src/main/java/com/social_media_back/vo/NewPostReq.java
@@ -1,47 +1,8 @@
 package com.social_media_back.vo;
 
-import java.time.LocalDate;
-
 import com.social_media_back.entity.PostInfo;
 
 public class NewPostReq extends PostInfo {
-	
-//	private int userId;
-//	
-//	private String postContent;
-//	
-//	private String postImage;
-//	
-//	private LocalDate createTime;
-//
-//	public NewPostReq() {
-//		super();
-//	}
-//
-//	public NewPostReq(int userId, String postContent, String postImage, LocalDate createTime) {
-//		super();
-//		this.userId = userId;
-//		this.postContent = postContent;
-//		this.postImage = postImage;
-//		this.createTime = createTime;
-//	}
-//
-//	public int getUserId() {
-//		return userId;
-//	}
-//
-//	public String getPostContent() {
-//		return postContent;
-//	}
-//
-//	public String getPostImage() {
-//		return postImage;
-//	}
-//
-//	public LocalDate getCreateTime() {
-//		return createTime;
-//	}
-	
 	
 	
 }
